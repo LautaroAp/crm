20dddfe51f506a034832231935ab68a882a2a011
LautaroAp <lautaro.ap@gmail.com>
2019-03-27 16:19:53 -0300

    commit

--- public/js/bienes.js
+++ public/js/bienes.js
@@ -1,25 +1,29 @@
-var items=[];
+var items = [];
+
 function addItems(bienes) {
     items = bienes;
-    console.log(items);
-    var col = ["Nombre", "Descripcion", "Precio", "Descuento", "Iva", "Subtotal"];
-    //TABLE HEADER
     var table = document.createElement("table");
     table.setAttribute("id", "table_bienes");
-    table.setAttribute("class", "table table-hover");
-    table.setAttribute("role","button");
-    var tr = table.insertRow(-1);                   // TABLE ROW.
+    table.setAttribute("class", "display");
+
+    var thead = document.createElement("thead");
+    var col = ["Nombre", "Descripcion", "Precio", "Descuento", "Iva", "Subtotal"];
+    var tr = thead.insertRow(-1);                   
     for (var i = 0; i < col.length; i++) {
-        var th = document.createElement("th");      // TABLE HEADER.
+        var th = document.createElement("th");      
         th.innerHTML = col[i];
         tr.appendChild(th);
     }
-    //TABLE BODY
+    thead.appendChild(tr);
+    table.appendChild(thead);
+
+    var tbody = document.createElement("tbody");
+    tbody.setAttribute("role", "button");
     var value = null;
     for (var i = 0; i < bienes.length; i++) {
         var item = bienes[i]
-        tr = table.insertRow(-1);
-        // tr.onclick= selectItem(item["id"]);
+        tr = tbody.insertRow(-1);
+        tr.onclick= selectItem(item["id"]);
         tr.setAttribute("id", i);
         tr.setAttribute("class", "click");
         tr.setAttribute("onclick","selectItem(event,id)");
@@ -29,22 +33,20 @@ function addItems(bienes) {
             tabCell.setAttribute("id", i);
             tabCell.setAttribute("class", "click");
             value = item[col[j]];
-            
-            // if ((col[j] == "Descuento") || (col[j] == "Iva")){value = formatPercent(value);}
             if (col[j] == "Descuento"){value = formatPercent(value);}
-
             if (col[j] == "Precio"){value = formatMoney(value);}
-            
             tabCell.innerHTML = value;
         }
     }
+    table.appendChild(tbody);
+
     var divContainer = document.getElementById("contenido_bienes");
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
 }
 
 function formatMoney(number) {
-    return '$ '+ number.toLocaleString('en-US');
+    return '$ ' + number.toLocaleString('en-US');
 }
 
 function formatPercent(number) {
@@ -52,13 +54,13 @@ function formatPercent(number) {
 }
 
 var item = null;
-var item_ant=null;
+var item_ant = null;
 // obtengo ID de eventos
-function selectItem(e,pos) {
+function selectItem(e, pos) {
     $('#' + pos).toggleClass('item-seleccion');
     if ($('#' + pos).hasClass('item-seleccion')) {
-        item_ant=item;
-        if (item_ant){
+        item_ant = item;
+        if (item_ant) {
             $('#' + item_ant).toggleClass('item-seleccion');
         }
         // Guardo id de la fila seleccionada
@@ -81,16 +83,16 @@ function selectItem(e,pos) {
         $("#subtotal").val('0.00');
         $("#idbien").val('0');
     }
-   
+
 }
 
 // TODA UNA TARDE TRATANDO DE HACER ESTA PUTA FUNCION!!!! FALTA VERIFICAR
-function seleccionaIva(iva_select){
+function seleccionaIva(iva_select) {
     // console.log(iva_select);
     tabla_ivas = $("#iva option");
     for (var i = 0; i < tabla_ivas.length; i++) {
         var opt = tabla_ivas[i];
-        if(opt.innerHTML.trim() == iva_select.trim()){
+        if (opt.innerHTML.trim() == iva_select.trim()) {
             opt.setAttribute("selected", "true");
         } else {
             opt.removeAttribute("selected");
